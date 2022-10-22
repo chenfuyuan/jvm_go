3f5bf7c2ff0bb970f4215a044ab07532d0fd601c
chenfuyuan <chenfuyuan@xihuanwu.com>
2022-10-22 20:05:54 +0800

    feat: 增加注释并抽离各个方法

--- src/main/cmd.go
+++ src/main/cmd.go
@@ -15,9 +15,27 @@ type Cmd struct {
 }
 
 func parseCmd() *Cmd {
-	cmd := &Cmd{}           //初始化
-	flag.Usage = printUsage //
+	cmd := &Cmd{} //初始化
+	bindVar(cmd)
 
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0] //第一个参数为加载文件
+		cmd.args = args[1:] //其余参数为参数文件
+	}
+
+	return cmd
+}
+
+func getArgs(cmd *Cmd) {
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+}
+func bindVar(cmd *Cmd) {
+	flag.Usage = printUsage
 	//绑定命令行参数
 	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
 	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
@@ -26,14 +44,6 @@ func parseCmd() *Cmd {
 	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
 	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
 	flag.Parse()
-
-	args := flag.Args()
-	if len(args) > 0 {
-		cmd.class = args[0]
-		cmd.args = args[1:]
-	}
-
-	return cmd
 }
 
 func printUsage() {
--- src/main/mian.go
+++ src/main/mian.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	cmd := parseCmd()
+	//经测试，flag先解析[-option] 再解析args
+	println("cmd.class=", cmd.class, "cmd.version=", cmd.versionFlag, "cmd.helpFlag=", cmd.helpFlag)
 	if cmd.versionFlag {
 		fmt.Println("version 0.0.1")
 	} else if cmd.helpFlag || cmd.class == "" {
